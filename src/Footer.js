ccb8994d87d54d8bd500cdb0c1c47c3668bda827
chevonnesmellie <95383005+chevonnesmellie@users.noreply.github.com>
2022-02-04 17:27:26 -0500

    added a rel value to link

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,6 @@ import React from "react";
 export default function Footer() {
     return (
     <footer class="Footer">
-        <a href="https://github.com/chevonnesmellie/weather-app" target="_blank">Open source code</a> by Chevonne Smellie
+        <a href="https://github.com/chevonnesmellie/weather-app" target="_blank" rel="noreferrer">Open source code</a> by Chevonne Smellie
     </footer>)
 }
