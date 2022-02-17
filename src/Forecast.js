1cbbb8ebb8e13a338c164da03020fe05680fd3a1
chevonnesmellie <95383005+chevonnesmellie@users.noreply.github.com>
2022-02-17 13:41:15 -0500

    added useEffect function to update forecast; removed conversion feature

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ForecastDay from './ForecastDay';
 
@@ -7,6 +7,10 @@ import "./Forecast.css";
 export default function Forecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecastData, setForecastData] = useState(null);
+
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
 
     function handleForecastResponse(response) {
         setForecastData(response.data.daily);
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -3,34 +3,9 @@ import React, { useState } from "react";
 import "./WeatherTemperature.css"
 
 export default function WeatherTemperature(props) {
-    let [unit, setUnit] = useState("celsius");
-
-    function showFahrenheit(event) {
-        event.preventDefault();
-        setUnit("fahrenheit");
-    }
-
-    function showCelsius(event) {
-        event.preventDefault();
-        setUnit("celsius");
-    }
-
-    function fahrenheit() {
-        return props.celsiusTemp * 9 /5 + 32;
-    }
-
-    if (unit === "celsius") {
         return (
             <div className="WeatherTemperature">
-                <span className="current-temp">{Math.round(props.celsiusTemp)}<span className="units">°C | <a href="/" onClick={showFahrenheit}>°F</a></span></span>
+                <span className="current-temp">{Math.round(props.celsiusTemp)}<span className="units">°C</span></span>
             </div>
-        ) 
-    } else {
-        return (
-            <div className="WeatherTemperature">
-                <span className="current-temp">{Math.round(fahrenheit())}<span className="units"><a href="/" onClick={showCelsius}>°C </a> | °F</span></span>
-            </div>
-        )
-    }
-
+        );
 }
